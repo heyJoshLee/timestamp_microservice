d977443555e23c43c6161523e90d2a525168b710
jleewebdev <lee45276@gmail.com>
2016-03-17 23:32:05 +0800

    V1.0 complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+var months = ["January", "February", "March",
+              "April", "May", "June", "July",
+              "August", "September", "October",
+              "November", "December"];
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday",
+            "Thursday", "Friday", "Saturday"];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Simple Timestamp' });
+});
+
+router.get('/:input', function(req, res, next) {
+  var input = req.params["input"],
+  output = {"unix": null,
+            "natural": null}
+
+
+  if(input.match(/^\d+$/)) {
+    input = +input;
+  }
+  var d = new Date(input);
+
+  if (Object.prototype.toString.call(d) === "[object Date]") {
+    if ( isNaN(d.getTime())) { res.send(output); }
+
+    output["unix"] = d.getTime();
+    output["natural"] = days[d.getDay()] + " " + d.getDate() +  ", " + d.getFullYear();
+  } 
+
+  res.send(output);
 });
 
 module.exports = router;
